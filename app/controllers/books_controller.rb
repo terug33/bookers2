fd286b08dcb5978528b85235ec9578f037db6066
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-05-28 15:55:32 +0000

    4-27-0055

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  # before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @books= Book.all
@@ -56,5 +57,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end 
+
+  # def ensure_correct_user
+  # @book = Book.find(params[:id])
+  # unless @book.user == current_user
+  #   flash[:alert] = "他人の投稿は編集できません。"
+  #   redirect_to books_path
+  # end
 
 end
